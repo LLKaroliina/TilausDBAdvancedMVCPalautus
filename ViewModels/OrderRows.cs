af516b1e02159c374a7a6c8f257e39ece2a23f5f
Karoliina Lemmetti <karoliina.lemmetti@gmail.com>
2024-03-13 10:15:49 +0200

    Add project files.

---
+++ ViewModels/OrderRows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TilausDBAdvancedMVCPalautus.ViewModels;
+
+namespace TilausDBAdvancedMVCPalautus.ViewModels
+{
+    public class OrderRows
+    {
+        public int TilausID { get; set; }
+        public int TuoteID { get; set; }
+        public float Ahinta { get; set; }
+        public string Postitoimipaikka { get; set; }
+        public string Nimi { get; set; }
+        public int Maara { get; set; }
+        public int TilausriviID { get; set; }
+    }
+}
